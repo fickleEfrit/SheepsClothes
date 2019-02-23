8638487ff9009eaad102a353aee02390af5f4e1c
justing0927 <jag2724@g.rit.edu>
2019-02-23 16:28:44 -0500

    ye

--- assignScenario.py
+++ assignScenario.py
@@ -2,20 +2,18 @@ from flask import Flask
 import json
 import random
 
+
+
 def assignScenario(fileName):
     with open(fileName) as f:
         data = json.load(f)
     rand = random.randint(0, 100)
     sc = data["scenarios"][rand]
-
+    return sc
 
 
 def selectWolf(uids):
     rand = random.randint(0, len(uids))
+    wolfId = uids[rand]
+    return wolfId
 
-
-
-
-
-if __name__ == '__main__':
-    app.run()
