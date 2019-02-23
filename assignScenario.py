9811f2099c0b79cb5dc7c8ca52507d4f8e793621
justing0927 <jag2724@g.rit.edu>
2019-02-23 16:18:15 -0500

    ye

--- assignScenario.py
+++ assignScenario.py
@@ -1,14 +1,21 @@
 from flask import Flask
+import json
+from pprint import pprint
+
+with open('data.json') as f:
+    data = json.load(f)
+
+pprint(data)
 
 @app.route('/')
 
-def assignScenario():
+def assignScenario(fileName):
 
 
 def selectWolf():
 
 
-def
+
 
 if __name__ == '__main__':
     app.run()
