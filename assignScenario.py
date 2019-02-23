cdab41fe3ded12019f9a96db65ecc1fdee136054
justing0927 <jag2724@g.rit.edu>
2019-02-23 17:05:22 -0500

    ye

--- assignScenario.py
+++ assignScenario.py
@@ -4,14 +4,11 @@ from flask_sse import sse
 import json
 import random
 
+"""
 app = Flask(__name__)
 app.config["REDIS_URL"] = "redis://localhost"
 app.register_blueprint(sse, url_prefix='/stream')
-
-@app.route('/hello')
-def publish_hello():
-    sse.publish({"message": "Hello!"}, type='greeting')
-    return "Message sent!"
+"""
 
 def assignScenario(fileName, uids):
     with open(fileName) as f:
@@ -23,10 +20,12 @@ def assignScenario(fileName, uids):
 
     randH = random.randint(0,5)
     hint = data["scenarios"][randSc]["hints"][randH]
-
-    sse.publish({"active_users": 100}, channel="analytics")
-    sse.publish({"user": "alice", "status": "Life is good!"}, channel="users.social")
-
+    sse.publish({"user": wolfID, "message": "You are the wolf! Your hint is: " + hint}, type='greeting')
+    for x in uids.keys:
+        if x == wolfID:
+            continue
+        else:
+            sse.publish({"user": x, "message": "You are a sheep! You are in: " + hint}, type='greeting')
 
 
 
