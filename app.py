a5b5e27ba61d1d131f6a040791224861dd1e560f
Jonah Fritz <powerhungry7@gmail.com>
2019-02-23 20:09:59 -0500

    handle scenario selection

--- app.py
+++ app.py
@@ -5,14 +5,16 @@ from flask import session
 from flask import redirect
 from flask import url_for
 from random import randint
+import json
 
 app = Flask(__name__)
 app.secret_key = 'smklADSF,l32'
 users = []
 roles = {}
-
 game_in_progress = False
-
+hint = ''
+scenario = ''
+file_name = 'word_bank.json'
 
 def assignRoles():
    wolfID = selectWolf(users)
@@ -28,6 +30,16 @@ def selectWolf(uids):
    wolfId = uids[rand]
    return wolfId
 
+
+def establishScenario():
+    with open(file_name, 'r') as f:
+        data = json.load(f)
+    scenario_number = randint(0, len(data) - 1)
+    hint_number = randint(0, 2)
+    global scenario
+    scenario = data['wordbank'][scenario_number]['tword']
+    global hint
+    hint = data['wordbank'][scenario_number]['words'][hint_number]
 
 
 @app.route('/', methods=['GET'])
@@ -54,15 +66,15 @@ def lobby():
 
 @app.route('/game', methods=['GET'])
 def game():
-    print(session)
-    print(roles)
-    return render_template('game.html', roles=roles)
+    return render_template('game.html', roles=roles, hint=hint, scenario=scenario)
 
 
 @app.route('/start_game', methods=['POST'])
 def start_game():
-    assignRoles()
+    global game_in_progress
     game_in_progress = True
+    assignRoles()
+    establishScenario()
     return redirect(url_for('game'))
 
 
